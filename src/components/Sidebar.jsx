3fe381061b5c797c98f49a20d45a38d4833564be
Veronica Rojas <ar_veronica@yahoo.com>
2023-09-20 18:31:23 +0100

    add delete button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,42 @@
 import React from "react"
+import {useState} from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export default function Sidebar(props) {
+    const [hoveredNotes, setHoveredNotes] = useState({});
+
+    const handleMouseOver = (noteId) => {
+        setHoveredNotes((prevHoveredNotes) => ({
+            ...prevHoveredNotes,
+            [noteId]: true
+        }))
+    }
+
+    const handleMouseOut = (noteId) => {
+        setHoveredNotes((prevHoveredNotes) => ({
+            ...prevHoveredNotes,
+            [noteId]: false
+        }))
+    }
+
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
+            onMouseOver={() => handleMouseOver(note.id)}
+            onMouseOut={() => handleMouseOut(note.id)}
+
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split(/[\r\n]+/)[0] ? note.body.split(/[\r\n]+/)[0] : "Blank Note"}</h4>
+                { hoveredNotes[note.id] === true &&
+                <button className="delete--button"
+                >
+                <FontAwesomeIcon icon={faTrash} />
+                </button> }
             </div>
         </div>
     ))
