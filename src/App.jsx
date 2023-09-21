e07cbaedde603dc6a00ae5f0a69ffc0bca7943be
Veronica Rojas <ar_veronica@yahoo.com>
2023-09-21 14:40:37 +0100

    remove data file and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 import './App.css'
